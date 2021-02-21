6363d39ec63e868c41b447a1faec612403ae5ba6
animalise <72106785+animalise@users.noreply.github.com>
2021-02-20 23:18:08 -0500

    add link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1>Do I Need A Jacket?</h1>
       <Weather  />
+      <small><a href="https://github.com/animalise/jacket-project-react">open source code</a> by analise rios</small>
     </div>
   );
 }
