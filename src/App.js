dccdcd75ccec09c0feeb721d29be86799d4378bb
animalise <72106785+animalise@users.noreply.github.com>
2021-02-21 19:10:58 -0500

    add search form + some styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Weather from "./Weather";
 import './App.css';
 
@@ -7,7 +6,9 @@ function App() {
     <div className="App">
       <h1>Do I Need A Jacket?</h1>
       <Weather  />
+      <div className="footer">
       <small><a href="https://github.com/animalise/jacket-project-react">open source code</a> by analise rios</small>
+    </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,25 @@
-import react from "react";
+import react, {useState} from "react";
 import axios from "axios";
 import './Weather.css';
 
 export default function Weather(props) {
   return (
-    <p> hello world! </p>
+    <div className="searchForm">
+      <form>
+        <input type="search"
+        placeholer="Enter a city..."
+        className="formControl"
+        autoComplete="off"
+        autoFocus="true"
+        />
+       
+        <button type="button"
+          className="btn btn-outline-dark">Search</button>
+           
+        <button type="button" 
+          className="btn btn-outline-light">Current Location</button>
+      </form>
+      
+    </div>
   )
 }
