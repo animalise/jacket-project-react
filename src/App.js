d0e1fbbf45886b619e63ffebab1f0f0c5e1b1882
animalise <72106785+animalise@users.noreply.github.com>
2021-02-21 22:53:14 -0500

    add API integration

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Do I Need A Jacket?</h1>
-      <Weather  />
+      <Weather  jacket="YES!"/>
       <div className="footer">
-      <small><a href="https://github.com/animalise/jacket-project-react">open source code</a> by analise rios</small>
     </div>
+      <small><a href="https://github.com/animalise/jacket-project-react">open source code</a> by analise rios</small>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,62 @@
-import react, {useState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import './Weather.css';
 
 export default function Weather(props) {
   const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
 
-  function updateCity(response) {
-    setCity(response.target.value);
+  function updateCityData(event) {
+    setCity(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     let apiKey = "af173d370d3263e90c511e8cd78a494a";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(updateCity);
+    axios.get(apiUrl).then(handleResponse);
   }
-  
-  return (
-    <div className="searchForm">
-      <form onSubmit={handleSubmit}>
-        <input type="search"
-        placeholer="Enter a city..."
-        className="formControl"
-        autoComplete="off"
-        autoFocus="true"
-        onChange={updateCity}
-        />
-       
-        <button type="button"
-          className="btn btn-outline-dark">Search</button>
-           
-        <button type="button" 
-          className="btn btn-outline-light">Current Location</button>
-      </form>
-      <div className="weatherInfo">
-      <h2> {city} </h2>
+
+  function handleResponse(response) {
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      wind: response.data.wind,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+    console.log(response.data);
+  }
+
+    return (
+      <div className="searchForm">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholer="Enter a city..."
+            autoComplete="off"
+            autoFocus={true}
+            onChange={updateCityData}
+          />
+          <button type="button" className="btn btn-outline-dark">
+            Search
+          </button>
+          <button type="button" className="btn btn-outline-light">
+            Current Location
+          </button>
+        </form>
+        <div className="weatherInfo">
+          <ul>
+            <li>
+              <h2> {city} </h2>
+            </li>
+            <li>Temperature: {weather.temperature}°F </li>
+            <li>Description: {weather.description}</li>
+            <li>Do I need a Jacket? {props.jacket}</li>
+            <li>
+              <img src={weather.icon} alt="weather icon"></img>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  )
-}
+    ); 
+  }
